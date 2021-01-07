3c39f8adbee41e22de8f70c6e541fed8e0cb9666
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-01-07 18:16:14 +0900

    Add: DriverPage 생성

---
+++ src/page/DriverPage/DriverPage.tsx
@@ -0,0 +1,13 @@
+import PageTemplate from 'components/PageTemplate/PageTemplate';
+import React from 'react';
+
+const DriverPage = (): JSX.Element => {
+
+  return (
+    <PageTemplate>
+      <DriverContainer />
+    </PageTemplate>
+  )
+}
+
+export default DriverPage;
