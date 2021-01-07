02ed87a136421720d563c43e997b77fa0069f4bf
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-01-07 17:44:46 +0900

    Add: PageTemplate

---
+++ src/components/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './PageTemplate.scss';
+import logo from 'assets/logo.svg';
+interface IPageTemplate {
+  children: React.ReactNode;
+}
+
+const PageTemplate = ({ children }: IPageTemplate) => {
+  return (
+    <div className="PageTemplate">
+      <div className="PageTemplate-NavBar">
+        <img src={logo} alt={logo} />
+      </div>
+      <div className="PageTemplate-Children">{children}</div>
+    </div>
+  );
+};
+
+export default PageTemplate;
