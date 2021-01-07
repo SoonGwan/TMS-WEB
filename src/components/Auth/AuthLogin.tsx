25b162fe47fe383a4116673469dab67df4ff874b
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-01-07 19:27:28 +0900

    Add: 로그인 타이틀, 팔레트 색상 추가

---
+++ src/components/Auth/AuthLogin.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AuthLogin.scss';
+
+const AuthLogin = () => {
+  return (
+    <div className="AuthLogin">
+      <div className="AuthLogin-Wrapper">
+        <div className="AuthLogin-Wrapper-Title">관리자 로그인</div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLogin;
