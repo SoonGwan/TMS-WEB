736bd634aad5124dca0deb6926e2d133b705c678
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-01-08 10:06:56 +0900

    Fix: Medium 버튼의 className을 지정하기 위해 스타일을  inline 형식으로 변경

--- src/components/common/MediumBtn/MediumBtn.tsx
+++ src/components/common/MediumBtn/MediumBtn.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import palette from 'styles/palette';
-
-import './MediumBtn.scss'
 
 interface IMediumBtn {
   title?: string;
   onClick?: React.MouseEventHandler,
   color?: string;
   fontColor?: string;
+  className?: string;
 }
 
 const MediumBtn = ({
   title,
   onClick,
+  className = 'MediumBtn',
   color = palette.blue_F4F7FB,
   fontColor = palette.black_000000 }: IMediumBtn) => {
 
+  const styles: CSSProperties = {
+    backgroundColor: color,
+    color: fontColor,
+    fontSize: '12px',
+    minWidth: '5rem',
+    padding: '8px',
+    border: 'none',
+    cursor: 'pointer',
+  }
+
   return (
     <button
-      className="MediumBtn-btn"
+      onFocus={e => e.target.style.outline = 'none'}
+      className={className}
       onClick={onClick}
-      style={{
-        backgroundColor: color,
-        color: fontColor,
-      }}
+      style={styles}
     >{title}</button>
   )
 }
