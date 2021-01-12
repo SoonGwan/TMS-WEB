105f976cdc44977a7675c40844feeba80a42e7af
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-01-12 14:59:16 +0900

    Add: 로그인 페이지 추가

---
+++ src/page/AuthPage/AuthPage.tsx
@@ -0,0 +1,8 @@
+import AuthLoginContainer from 'container/AuthContainer/AuthLoginContainer';
+import React from 'react';
+
+const AuthPage = () => {
+  return <AuthLoginContainer />;
+};
+
+export default AuthPage;
