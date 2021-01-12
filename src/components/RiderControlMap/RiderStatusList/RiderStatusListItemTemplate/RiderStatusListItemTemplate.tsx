614372d48e4094b2bf69465a67d6147df6e38cd4
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-01-12 14:29:03 +0900

    Fix: 컴포넌트 이름 변경

--- src/components/RiderControlMap/RiderStatusList/RiderStatusListItemTemplate/RiderStatusListItemTemplate.tsx
+++ src/components/RiderControlMap/RiderStatusList/RiderStatusListItemTemplate/RiderStatusListItemTemplate.tsx
@@ -5,29 +5,29 @@ import { useRecoilValue } from 'recoil';
 import './RiderStatusListItemTemplate.scss';
 import { RiderStatus } from 'enum/RiderStatusList';
 
-interface IRiderStatusListItemTemp {
+interface IRiderStatusListItemTemplate {
   id: number;
   state: number;
   adress: string;
   time: string;
 }
 
-const RiderStatusListItemTemp = () => {
+const RiderStatusListItemTemplate = () => {
   const driver = useRecoilValue(DriverDeliveryState);
-  const driverList = driver.map((data: IRiderStatusListItemTemp) => {
+  const driverList = driver.map((data: IRiderStatusListItemTemplate) => {
     const { id, state, adress, time } = data;
     return (
       <>
-        <div className="RiderStatusListItemTemp">
-          <div className="RiderStatusListItemTemp-Idx">{id}</div>
-          <div className="RiderStatusListItemTemp-Status">
+        <div className="RiderStatusListItemTemplate">
+          <div className="RiderStatusListItemTemplate-Idx">{id}</div>
+          <div className="RiderStatusListItemTemplate-Status">
             <StatusLabel
               text={state === RiderStatus.WAITING ? '배차 대기' : '배차 완료'}
               status={state === RiderStatus.WAITING ? 'normal' : 'warning'}
             />
           </div>
-          <div className="RiderStatusListItemTemp-Adress">{adress}</div>
-          <div className="RiderStatusListItemTemp-Time">{time}</div>
+          <div className="RiderStatusListItemTemplate-Adress">{adress}</div>
+          <div className="RiderStatusListItemTemplate-Time">{time}</div>
         </div>
       </>
     );
@@ -35,4 +35,4 @@ const RiderStatusListItemTemp = () => {
   return <>{driverList}</>;
 };
 
-export default RiderStatusListItemTemp;
+export default RiderStatusListItemTemplate;
