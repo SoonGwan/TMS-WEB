5e0649f6d80ec7c3b96ec79de4a5b10b13649832
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-01-12 15:54:56 +0900

    Add: 페이지 네비게이션 아이콘 추가

--- src/components/PageTemplate/PageTemplate.tsx
+++ src/components/PageTemplate/PageTemplate.tsx
@@ -1,16 +1,55 @@
 import React from 'react';
-import './PageTemplate.scss';
+import classNames from 'classnames/bind';
 import logo from 'assets/logo.svg';
+import { Link, useLocation } from 'react-router-dom';
+import { ClassNamesFn } from 'classnames/types';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { ImProfile } from 'react-icons/im';
+
 interface IPageTemplate {
   children: React.ReactNode;
 }
+
+const style = require('./PageTemplate.scss');
+const cx: ClassNamesFn = classNames.bind(style);
+
 const PageTemplate = ({ children }: IPageTemplate) => {
+  const { pathname } = useLocation();
+
   return (
-    <div className="PageTemplate">
-      <div className="PageTemplate-NavBar">
+    <div className={cx('PageTemplate')}>
+      <div className={cx('PageTemplate-NavBar')}>
+        <div className={cx('PageTemplate-NavBar-ItemsWrapper')}>
+          <Link
+            to={'/'}
+            className={cx('PageTemplate-NavBar-ItemsWrapper-Link')}
+          >
+            <div
+              className={cx('PageTemplate-NavBar-ItemsWrapper-Items', {
+                'PageTemplate-NavBar-ItemsWrapper-Items-selectedItem':
+                  pathname === '/',
+              })}
+            >
+              <HiOutlineLocationMarker />
+            </div>
+          </Link>
+          <Link
+            to={'/member'}
+            className={cx('PageTemplate=NavNar-ItemsWrapper-Link')}
+          >
+            <div
+              className={cx('PageTemplate-NavBar-ItemsWrapper-Items', {
+                'PageTemplate-NavBar-ItemsWrapper-Items-selectedItem':
+                  pathname === '/member',
+              })}
+            >
+              <ImProfile />
+            </div>
+          </Link>
+        </div>
         <img src={logo} alt={logo} />
       </div>
-      <div className="PageTemplate-Children">{children}</div>
+      <div className={cx('PageTemplate-Children')}>{children}</div>
     </div>
   );
 };
